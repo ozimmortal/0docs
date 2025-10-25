05d4a64fa6669a7e77a31f9b635bc4640049651a
oz <zaleambo@gmail.com>
2025-10-25 22:42:42 +0300

    adds theme and mdx configuration

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Home() {
   return (
     <div>
-      
+      <ModeToggle/>
+
     </div>
   );
 }
